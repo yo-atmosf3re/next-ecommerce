fbd015f02263d33700a2cdbbc7ec40f512437bec
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-10-08 20:51:56 +0300

    feat(backend): большая часть сида подготовлена и в рабочем состоянии. Начата подготовка и разработка api;

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,30 @@
+import { Prisma } from "@prisma/client";
 import { _ingredients, categories, products } from "./constants";
 import { prisma } from "./prisma-client";
 import { hashSync } from 'bcrypt';
+import { connect } from "http2";
+
+// ? Случайное десятичное число;
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const generateProductItem = ({
+  productId,
+  pizzaType,
+  size,
+}: {
+  productId: number;
+  pizzaType?: 1 | 2;
+  size?: 20 | 30 | 40;
+}) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    pizzaType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
 
 /**
  * Функция для генерации данных;
@@ -46,6 +70,7 @@ async function up() {
         '/assets/images/pizza_1.png',
       categoryId: 1,
       ingredients: {
+        // ? Указываем связь, для этой пиццы будет пять ингредиентов;
         connect: _ingredients.slice(0, 5),
       },
     },
@@ -74,13 +99,90 @@ async function up() {
       },
     },
   });
+
+  await prisma.productItem.createMany({
+    data: [
+        // ? Пицца "Пепперони фреш";
+      generateProductItem({ productId: pizza1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 40 }),
+
+      // ? Пицца "Сырная";
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 40 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 40 }),
+
+      // ? Пицца "Чоризо фреш";
+      generateProductItem({ productId: pizza3.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 40 }),
+
+      // ? Остальные продукты - генерация с разной стоимостью, но без размера и вариации;
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 })
+    ]
+  })
+
+  await prisma.cart.createMany(
+    {
+        data: [
+            {
+                userId: 1,
+                totalAmount: 0,
+                token: '12345'
+            },
+            {
+                userId: 2,
+                totalAmount: 0,
+                token: '54321'
+            },
+        ]
+    }
+  )
+
+  await prisma.cartItem.create({
+        data: 
+            {
+                productItemId: 1,
+                cartId: 1,
+                quantity: 2,
+                ingredients: {
+                    connect: [{id: 1}, {id: 2},{id: 3},]
+                }
+            }
+    }
+  )
 }
 
 /**
  * Функция для очистки данных перед генерацией для избавления от одних и тех же данных;
  */
 async function down() {
-  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
 }
 
 // ? $executeRaw — метод клиента Prisma для выполнения необработанных SQL-запросов;
