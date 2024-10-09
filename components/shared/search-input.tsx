32d73af7f09e6cd16a789155a66424b91996ea9e
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-10-09 22:54:57 +0300

    feat(frontend): создана часть клиентского api для работы с поиском по продуктам. Установлен и настроен axios. Доработана компонента SearchInput;

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,21 +1,44 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { Api } from '@/services/api-client';
+import { Product } from '@prisma/client';
 import { Search } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
+import { useClickAway } from 'react-use';
 
 interface SearchInputPropsI {
     className?: string;
 }
 
 export const SearchInput: React.FC<SearchInputPropsI> = ({ className }) => {
+    const [searchQuery, setSearchQuery] = React.useState('');
     const [focused, setFocused] = React.useState(false);
+    const [products, setProducts] = React.useState<Product[]>([]);
+    const ref = React.useRef(null);
+
+    useClickAway(ref, () => {
+        setFocused(false);
+    });
+
+    React.useEffect(() => {
+        Api.products.search(searchQuery);
+    }, []);
+
+    const onClickItem = () => {
+        setFocused(false);
+        setSearchQuery('');
+        setProducts([]);
+    };
+
     return (
         <>
             {focused && (
                 <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30" />
             )}
             <div
+                ref={ref}
                 className={cn(
                     'flex rounded-2xl flex-1 justify-between relative h-11 z-30',
                     className,
@@ -27,9 +50,31 @@ export const SearchInput: React.FC<SearchInputPropsI> = ({ className }) => {
                     type="text"
                     placeholder="Найти пиццу..."
                     onFocus={() => setFocused(true)}
-                    // value={searchQuery}
-                    // onChange={(e) => setSearchQuery(e.target.value)}
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
+            </div>
+            <div
+                className={cn(
+                    'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+                    focused && 'visible opacity-100 top-12',
+                )}
+            >
+                {products.map((product) => (
+                    <Link
+                        onClick={onClickItem}
+                        key={product.id}
+                        className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                        href={`/product/${product.id}`}
+                    >
+                        <img
+                            className="rounded-sm h-8 w-8"
+                            src={product.imageUrl}
+                            alt={product.name}
+                        />
+                        <span>{product.name}</span>
+                    </Link>
+                ))}
             </div>
         </>
     );
