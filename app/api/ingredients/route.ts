78a522adb99c2491949016a58235baedfce3ed06
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-10-09 09:22:21 +0300

    feat(backend): разработка api для поиска продуктов из БД;

--- app/api/ingredients/route.ts
+++ app/api/ingredients/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextResponse } from "next/server";
 
+// ? Запрос на получение всех ингредиентов;
 export async function GET() {
     const ingredients = await prisma.ingredient.findMany();
 
